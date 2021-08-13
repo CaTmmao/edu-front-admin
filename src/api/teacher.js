4e97d86e56222a0118151f05951b7b160415ecdf
catmmao <catmmao@gmail.com>
2021-08-13 23:37:19 +0800

    feat(views/teacher): 上传头像&讲师信息保存功能

--- src/api/teacher.js
+++ src/api/teacher.js
@@ -53,5 +53,20 @@ export default {
             method: 'post',
             data
         })
+    },
+
+    /**
+     * 上传图片
+     * @param file 图片
+     * @return {AxiosPromise} 图片存在oss上的地址
+     */
+    uploadPhoto(file) {
+        return request({
+            url: 'oss/file',
+            method: 'post',
+            data: file,
+            headers:{'Content-Type':'multipart/form-data'}
+        })
     }
+
 }
--- src/views/teacher/add.vue
+++ src/views/teacher/add.vue
@@ -20,6 +20,11 @@
         <el-input v-model="teacherInfo.intro" :rows="10" type="textarea"/>
       </el-form-item>
 
+      <el-form-item label="讲师头像">
+        <upload :img-url="teacherInfo.avatar" v-on:file-change="handleAvatarFileChange"
+                v-on:url-change="handleAvatarChange"></upload>
+      </el-form-item>
+
       <el-form-item>
         <el-button type="primary" @click="save">保存</el-button>
       </el-form-item>
@@ -29,23 +34,32 @@
 
 <script>
 import api from '@/api/teacher';
+import Upload from '@/components/Upload/index';
+
+// 讲师信息
+const teacherInfo = {
+  // 讲师名称
+  name: '',
+  // 讲师排序
+  sort: '0',
+  // 讲师头衔
+  level: 1,
+  // 讲师简介
+  intro: '',
+  // 讲师资历
+  career: '',
+  // 讲师头像
+  avatar: 'https://edu-site.oss-cn-beijing.aliyuncs.com/20212201335364831.jpg'
+}
 
 export default {
+  components: {Upload},
   data() {
     return {
       // 讲师信息
-      teacherInfo: {
-        // 讲师名称
-        name: '',
-        // 讲师排序
-        sort: '0',
-        // 讲师头衔
-        level: 1,
-        // 讲师简介
-        intro: '',
-        // 讲师资历
-        career: ''
-      }
+      teacherInfo: {},
+      // 头像文件
+      avatarFile: null
     }
   },
   created() {
@@ -55,13 +69,38 @@ export default {
     /**
      * 保存信息
      */
-    save() {
+    async save() {
       if (this.$route.params && this.$route.params.id) {
         let id = this.$route.params.id
+
+        if (this.avatarFile) {
+          await this.upload(this.avatarFile)
+        }
+
         this.updateTeacherById(id, this.teacherInfo);
       } else {
+        if (this.teacherInfo.avatar != teacherInfo.avatar) {
+          await this.upload(this.avatarFile)
+        }
+
         this.addTeacher(this.teacherInfo)
       }
+    },
+
+    /**
+     * 头像被更改了
+     * @param url 头像存在本地的地址
+     */
+    handleAvatarChange(url) {
+      this.teacherInfo.avatar = url
+    },
+
+    /**
+     * 头像被更改了
+     * @param file 文件信息
+     */
+    handleAvatarFileChange(file) {
+      this.avatarFile = file
     },
 
     /**
@@ -101,11 +140,28 @@ export default {
      * 数据初始化
      */
     init() {
-      if (this.$route.params && this.$route.params.id) {
-        this.teacherInfo = this.$route.query.data
+      let {params, query} = this.$route
+
+      if (params && params.id) {
+        this.teacherInfo = JSON.parse(query.data)
       } else {
-        this.teacherInfo = {}
+        this.teacherInfo = teacherInfo
       }
+    },
+
+    /**
+     * 上传头像到OSS中
+     * @param file 图片信息
+     */
+    async upload(file) {
+      const formData = new FormData();
+      formData.append('file', file)
+
+      await api.uploadPhoto(formData)
+          .then(res => {
+            let {url} = res.data
+            this.teacherInfo.avatar = url
+          })
     }
   },
   watch: {
