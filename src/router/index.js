d9f5785d31e37c649dd44e41180f054f246e898c
catmmao <catmmao@gmail.com>
2021-09-23 19:35:02 +0800

    chore: 删除不需要的

--- src/router/index.js
+++ src/router/index.js
@@ -42,18 +42,6 @@ export const constantRoutes = [
         hidden: true
     },
 
-    {
-        path: '/',
-        component: Layout,
-        redirect: '/dashboard',
-        children: [{
-            path: 'dashboard',
-            name: 'Dashboard',
-            component: () => import('@/views/dashboard/index'),
-            meta: {title: 'Dashboard', icon: 'dashboard'}
-        }]
-    },
-
      // 分类管理
      {
         path: '/category',
